934b087341dd6af191122b390bad95bfe9de307c
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-05-20 00:08:20 +0300

    task_1 task manager done

---
+++ task_1.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <ctime>
+#include <vector>
+
+class userAction
+{
+public:
+    std::string begin_ = "begin";
+    std::string end_ = "end";
+    std::string status_ = "status";
+    std::string close_ = "close";
+};
+
+struct data_manager
+{
+    std::string task_name = "none";
+    double task_time = 0;
+};
+
+int main() {
+
+    std::vector<data_manager> tasks_list = {};
+    data_manager current_task;
+    userAction user_action;
+    std::time_t time_start, time_end;
+    bool task_watchdog = false;
+    double delta_time = 0;
+    std::string user_input, user_discription;
+
+
+    while (user_input != user_action.close_)
+    {
+        std::cout << "Type action begin/end/status/close: ";
+        std::cin >> user_input;
+
+        if(user_input == user_action.begin_)
+        {
+            if(task_watchdog)
+            {
+                time_end = std::time(nullptr);
+                delta_time = difftime(time_end,time_start);
+                current_task.task_name = user_discription;
+                current_task.task_time = delta_time;
+                tasks_list.push_back(current_task);
+                task_watchdog = false;
+
+            }
+            std::cout << "type name for your task: " << std::endl;
+            std::cin >> user_discription;
+
+            time_start = std::time(nullptr);
+            task_watchdog = true;
+        }
+
+        if(user_input == user_action.end_)
+        {
+            time_end = std::time(nullptr);
+            delta_time = difftime(time_end,time_start);
+            current_task.task_name = user_discription;
+            current_task.task_time = delta_time;
+            tasks_list.push_back(current_task);
+            task_watchdog = false;
+        }
+
+        if(user_input == user_action.status_)
+        {
+            if(tasks_list.size() == 0) std::cout << "no finished tasks yet" << std::endl;
+            else
+            {
+                for(int i = 0; i < tasks_list.size(); i++)
+                {
+                    std::cout << tasks_list[i].task_name << " " << tasks_list[i].task_time << " seconds" << std::endl;
+                }
+                std::cout << std::endl;
+            }
+
+            if(task_watchdog)
+            {
+                std:: cout << "current task: " << user_discription << " "
+                    << difftime(std::time(nullptr),time_start) << std::endl;
+            }
+
+
+        }
+
+    }
+
+    std::cout << "exited" << std::endl;
+
+    return 0;
+}
+
+
