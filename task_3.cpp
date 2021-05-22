8afcb9186d6fa6acee8b1fa62c63fe5cf3e1065c
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-05-22 13:30:50 +0300

    task_3 timer done

--- task_3.cpp
+++ task_3.cpp
@@ -1,4 +1,34 @@
 #include <iostream>
 #include <ctime>
-#include <vector>
 #include <iomanip>
+
+int main()
+{
+    std::time_t timer_start;
+    std::time_t timer_finish;
+    std::time_t delta_sec;
+    std::time_t time = std::time(nullptr);
+    std::tm* local_time = std::localtime(&time);
+
+    std::cout << "type timer like minutes:seconds : ";
+    std::cin >> std::get_time(local_time, "%M:%S");
+
+    delta_sec = local_time->tm_min*60 + local_time->tm_sec;
+    time = std::time(nullptr);
+    local_time = std::localtime(&time);
+    timer_start = std::mktime(local_time);
+    timer_finish = timer_start + delta_sec;
+
+    while(true)
+    {
+        time = std::time(nullptr);
+        if(timer_finish - time <= 0)
+        {
+            std::cout << "ding!" << std::endl;
+            break;
+        }
+        std::cout << timer_finish - time << " seconds left" << std::endl;
+    }
+
+    return 0;
+}
